b5351cc7d02f020cb2134394b3346dc1ba03f3d6
mrthomasjackson <tjackson@TFJs-MacBook-Air.local>
2014-08-11 15:23:30 -0400

    Added script type

--- Expressions_Personal/js/personal.js
+++ Expressions_Personal/js/personal.js
@@ -11,9 +11,13 @@ alert("Testing 1,2,3");
 // converting inches to centimeters
 // Equation is 1in = 2.54cm*in
 
+<script type="text/javascript">
+
 var inches = prompt("Enter your height in 'inches'",50);
 var centimeters;
 
 centimeters = (2.54*inches);
 
 alert (centimeters);
+
+</script>
