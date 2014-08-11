554baa362ea448a2d857cd802c2ac80fac98d2c8
mrthomasjackson <tjackson@TFJs-MacBook-Air.local>
2014-08-11 11:21:26 -0400

    Week 2 Book Studies

---
+++ Week 2 Studies/js/java.js
@@ -0,0 +1,18 @@
+/**
+ * Created by tjackson on 8/11/14.
+ */
+var string1 = 'hello';
+var string2 = 'goodbye';
+
+alert (string1);
+alert (string2);
+
+string2 = string1;
+
+alert (string1);
+alert (string2);
+
+string1 = 'now for something different';
+
+alert(string1);
+alert(string2);
