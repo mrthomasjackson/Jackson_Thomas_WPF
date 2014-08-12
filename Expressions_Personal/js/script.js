e99d1a54c45a813b5c6782ff96d92ac01444f9b4
mrthomasjackson <tjackson@TFJs-MacBook-Air.local>
2014-08-12 19:30:24 -0400

    created variables to store inches and centimeters

--- Expressions_Personal/js/script.js
+++ Expressions_Personal/js/script.js
@@ -9,3 +9,12 @@
 
 // converting inches to centimeters
 //Equation is 1in = 2.54cm*in
+
+//Created variable to store inches
+var inches = prompt ("Enter your height in inches",50);
+
+//created a variable to store centimeters
+var centimeters;
+
+
+
