5f9f8a9a25c1523ec774bb207f84785e27616b2b
mrthomasjackson <tjackson@TFJs-MacBook-Air.local>
2014-08-14 19:45:13 -0400

    Created a variable to store 'b'

--- Expressions_Wacky/js/wacky.js
+++ Expressions_Wacky/js/wacky.js
@@ -10,12 +10,13 @@
 alert("The purpose of this application is to calculate the pythagorean theorem.")
 
 //creating an equation to calculate the pythagorean theorem
-var aVariable = prompt("Please enter the value of 'a':");
+var aVariable = prompt("Please enter the value of 'a': ", 1 );
 
 //console.log 'a'
 console.log("The value of a is " + aVariable );
 
-//
+//create a variable to store the value of 'b'
+var bVariable = prompt("Please enter the value of 'b': ", 2);
 
 
 
