821413e3647dcf608c78b40db12dd17a0d3a0ced
mrthomasjackson <tjackson@TFJs-MacBook-Air.local>
2014-08-13 21:30:55 -0400

    Edit in comments

--- Expressions_Industry/js/industry.js
+++ Expressions_Industry/js/industry.js
@@ -31,25 +31,58 @@ alert("Based upon your desired salary of $"+salary+". After associated costs of 
 
 //create variables for holidays, vacation, and sick days
 alert ("To gauge the amount of billable hours you have, we have a few more questions:");
+
+//create a variable to hold holidays in the day format
 var holiday = prompt("How many holiday days do you plan on taking?",7);
+
+//console.log holiday
 console.log("The user plans on taking "+holiday+" days for holiday leave.");
+
+//create a variable for vacation in the day format
 var vacation = prompt ("How many vacation days do you plan on having?",14);
+
+//console.log vacation
 console.log ("The user plans on taking"+ vacation+" vacation days.");
+
+//create a variable for sick days in the day format
 var sick = prompt ("How many sick days do anticipate on having?",5);
+
+//console.log sick
 console.log("The user plans on having "+sick+ " sick days.");
 
+//creating a variable to calculate holiday hours
 var holidayHours = (holiday * 24);
+
+//console.log holidayHours
 console.log ("Holidays equals "+holidayHours+" hours.");
+
+//create a variable to calculate vacation hours
 var vacationHours = (vacation * 24);
+
+//console.log vacation hours
 console.log("Vacation days equals "+vacationHours+ " hours.");
+
+//create a variable to calculate sick hours
 var sickHours = (sick * 24);
+
+//console.log sickHours
 console.log("Sick days equals "+sickHours+" hours.");
 
+//create a variable to calculate total hours available to work
 var totalHours = (2920 - holidayHours - vacationHours - sickHours);
+
+//alert the user how many hours to charge per year
 alert ("Based upon 8 hour workdays, you have " + totalHours + " hours of work available a year.");
+
+//console.log totalHours
 console.log("User has " + totalHours + " hours of work anticipated per year.");
 
+//create a variable to calculate how much the user should be making per hour
 var hourlyWage = (totalHours * totalSalary);
+
+//alert the user how much they should charge per hour
 alert ("Based upon your estimated salary of " + totalSalary + " you should be charging $" + hourlyWage + " per hour.")
+
+//console.log results of hourlyWage
 console.log("The user should charge $" + hourlyWage + " per hour.");
 
