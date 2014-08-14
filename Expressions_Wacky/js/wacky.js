b53739c30d5ea53b87a4ca03cd8d4c6422da292b
mrthomasjackson <tjackson@TFJs-MacBook-Air.local>
2014-08-14 19:39:29 -0400

    created an initial alert to explain

--- Expressions_Wacky/js/wacky.js
+++ Expressions_Wacky/js/wacky.js
@@ -6,13 +6,16 @@
 
 //alert("Testing 1,2,3");
 
+//alert the user what the purpose of this application is
+alert("The purpose of this application is to calculate the pythagorean theorem.")
+
 //creating an equation to calculate the pythagorean theorem
 var aVariable = prompt("Please enter the value of 'a':");
 
 //console.log 'a'
 console.log("The value of a is " + aVariable );
 
-
+//
 
 
 
