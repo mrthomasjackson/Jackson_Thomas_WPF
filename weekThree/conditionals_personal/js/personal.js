cce44dce3dca36fdb04b3f72ccb5c41e53aacc7a
mrthomasjackson <tjackson@TFJs-MacBook-Air.local>
2014-08-20 20:52:51 -0400

    entered variable to store time and code
    to validate answers

--- weekThree/conditionals_personal/js/personal.js
+++ weekThree/conditionals_personal/js/personal.js
@@ -5,3 +5,17 @@
  */
 
 //alert("Testing 1,2,3");
+
+//should I get out of bed
+
+//create variable for time
+var currentTime = prompt("What is the current time? Please enter using format HHMM");
+
+if (currentTime === "" ) {
+    currentTime = prompt("What time is it?!?",0945);
+    }
+
+//console.log current time
+console.log("The time is currently " + currentTime);
+
+//create boolean variable to
