e3a837afa637ce53895a2690113e8c5ac5484a49
mrthomasjackson <tjackson@TFJs-MacBook-Air.local>
2014-08-21 16:23:20 -0400

    created variables to store height width and gratio

--- weekThree/conditionals_industry/js/industry.js
+++ weekThree/conditionals_industry/js/industry.js
@@ -12,5 +12,28 @@
 var name = prompt("Please enter your name:");
 
 //ternary operator to validate name
-(name === "") ? prompt("Please enter your name!"): (console.log("The user's name is " + name));
+(name === "") ? prompt("Please enter your name!") : alert ("Thank you for entering your name " + name);
+
+//console.log name
+console.log("The user's name is " + name);
+
+//create a variable to store height
+var height = prompt("Please enter the height of object in pixels: ");
+
+//console.log height
+console.log ("The height of the object is " + height + " pixels");
+
+var width = prompt("Please enter the width of the object in pixels: ");
+
+//console.log width
+console.log("The width of the object is " + width + " pixels");
+
+//the variable to store the golden ratio
+var goldenRatio = ((1 + math.sqrt(5))/2);
+
+//console.log golden ration
+console.log("The golden ratio is " + goldenRatio);
+
+
+
 
