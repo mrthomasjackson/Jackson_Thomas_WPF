a3d54d127a391df9f8c0af9573933acc9d6ad1fa
mrthomasjackson <tjackson@TFJs-MacBook-Air.local>
2014-08-20 21:38:44 -0400

    added comments to code

--- weekThree/conditionals_personal/js/personal.js
+++ weekThree/conditionals_personal/js/personal.js
@@ -21,12 +21,14 @@ console.log("The time is currently " + currentTime);
 //create variable to store if I am working or not
 var work = prompt("Are you working today?");
 
+//adding if statement to validate prompt
 if (work === "") {
     work = prompt("Are you working today?!?");
 }
 //declaring work as true or false
 if (work === "yes") {
     work = true;
+// adding else statement in case user is not working
 } else {
     work = false
 }
@@ -37,15 +39,21 @@ console.log ("Are you working? " + work);
 //tell the user to go back to bed if they are not working
 if (work === false) {
     alert("You do not have work, go back to bed.");
+//console.log the false statement of work
     console.log("User does not have work. Going back to bed.");
 }
 
+//creating second half of project
+//if user is working today
 if (work === true) {
 //ask the user what time work is
     var workTime = prompt("What time is work? Please enter using format HHMM: ");
+//checking the current time versus work time (using parseInt())
     if (parseInt(workTime) <= parseInt(currentTime)) {
+//alert and console.log if it is time to get out of bed
         alert ("Time to get out of bed!");
         console.log("It is time for the user to get out of bed.");
+//adding else statement if user can stay in bed
     } else {
         alert ("You can stay in bed until " + workTime);
         console.log("User has until " + workTime + " to sleep." );
