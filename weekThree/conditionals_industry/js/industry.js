9c39a7dbe99f701577a172037ab30216a07e5ea0
mrthomasjackson <tjackson@TFJs-MacBook-Air.local>
2014-08-21 16:05:37 -0400

    created a variable to store the name
    validated

--- weekThree/conditionals_industry/js/industry.js
+++ weekThree/conditionals_industry/js/industry.js
@@ -4,4 +4,13 @@
  Expressions_Industry
  */
 
-alert("Testing 1,2,3");
+//alert("Testing 1,2,3");
+
+//Create a script to calculate the ratio of the golden rectangle. Often used in the front end design of web sites.
+
+//create a variable to store users name
+var name = prompt("Please enter your name:");
+
+//ternary operator to validate name
+(name === "") ? prompt("Please enter your name!"): (console.log("The user's name is " + name));
+
