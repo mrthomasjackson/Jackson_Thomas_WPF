6c894b97c39fd8f575d0e3764a8d537669ec623f
mrthomasjackson <tjackson@TFJs-MacBook-Air.local>
2014-08-20 20:31:12 -0400

    Created folders subfolders and files

---
+++ weekThree/conditionals_industry/js/industry.js
@@ -0,0 +1,7 @@
+/*
+ Thomas Jackson
+ WPF 201417 Sec 5
+ Expressions_Industry
+ */
+
+alert("Testing 1,2,3");
---
+++ weekThree/conditionals_personal/js/personal.js
@@ -0,0 +1,7 @@
+/*
+ Thomas Jackson
+ WPF 201417 Sec 5
+ Conditionals_Personal
+ */
+
+//alert("Testing 1,2,3");
